b1a7013425126621db543b00a11e50853cb89c83
m16a1 <a741su@gmail.com>
2011-07-05 09:23:46 +0400

    Download video

--- test/test.js
+++ test/test.js
@@ -1,15 +1,28 @@
-var yt     = require('../lib/youtube'),
-    assert = require('assert');
+var yt      = require('../lib/youtube'),
+    assert  = require('assert'),
+    fs      = require('fs');
 
-var testURL = 'http://www.youtube.com/watch?v=F6k8lTrAE2g';
+var testURL = 'http://www.youtube.com/watch?v=14RFYXMXGqQ';
 
 yt.getInfo(testURL, function(info) {
-	assert.equal(info.title, 'Node.js: JavaScript on the Server');
-	assert.equal(info.description, 'Google Tech Talk July 28, 2010 ABSTRACT Presented by Ryan Dahl, the creator of the node.JS open source project. It is well known that event loops rather than...');
+	assert.equal(info.title, 'Node.js + Socket.io = CPU activity monitor');
+	assert.equal(info.description, 'Created using socket.io and nodejs using the dstat command on linux. Graphing is done by rgraph, while the nodejs is the backend and socket io is helping wit...');
 	assert.equal(info.allow_ratings, '1');
-	assert.equal(info.author, 'GoogleTechTalks');
-	assert.equal(info.length_seconds, '3599');
-	assert.deepEqual(info.keywords, ['google', 'tech', 'talk', 'javascript']);
-	assert.equal(info.video_id, 'F6k8lTrAE2g');
-	assert.equal(info.thumbnail_url, 'http://i3.ytimg.com/vi/F6k8lTrAE2g/default.jpg');
+	assert.equal(info.author, 'wertyuiop408');
+	assert.equal(info.length_seconds, '28');
+	assert.deepEqual(info.keywords, ['nodejs', 'socketio', 'socket', 'io', 'cpu', 'activity', 'monitor', 'real', 'time']);
+	assert.equal(info.video_id, '14RFYXMXGqQ');
+	assert.equal(info.thumbnail_url, 'http://i2.ytimg.com/vi/14RFYXMXGqQ/default.jpg');
+
+	info.download('/tmp/' + info.author + ' - ' + info.title, {
+		fmt: '5',
+		onData: function (len, size) {
+			console.log('downloaded ' + len + ' bytes from ' + size);
+		},
+
+		onComplete: function (file) {
+			console.log('download complete');
+		}
+	});
+	
 });
